0239b6e51dca84fa50f851f30ad333ca839244a2
prashant272 <prashantkrjha12@gmail.com>
2026-02-05 17:26:18 +0530

    added media partner card

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
+
+import { inject } from "@vercel/speed-insights";
+
+//  Inject performance tracking (runs once)
+inject();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
